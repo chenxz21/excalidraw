f63cfb31ed88ca634937905a50467077a754ecfb
chenxz21 <62413032+chenxz21@users.noreply.github.com>
2022-05-04 17:45:18 +0800

    Update index.tsx

--- src/excalidraw-app/index.tsx
+++ src/excalidraw-app/index.tsx
@@ -189,15 +189,6 @@ const initializeScene = async (opts: {
 
 const PlusLinkJSX = (
   <p style={{ direction: "ltr", unicodeBidi: "embed" }}>
-    Introducing Excalidraw+
-    <br />
-    <a
-      href="https://plus.excalidraw.com/plus?utm_source=excalidraw&utm_medium=banner&utm_campaign=launch"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Try out now!
-    </a>
   </p>
 );
 
